f9314b48e9e2986cf9437451ce32acf9820f7e15
Della Duck 🦆 <aurelien.strimelle@bstorm.be>
2025-11-25 10:17:50 +0100

    Demo Event 🙃

--- demo-02-state/src/App.jsx
+++ demo-02-state/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import EventDemo from './components/EventDemo/EventDemo'
 import Header from './ui/Header/Header'
 
 function App() {
@@ -6,6 +7,10 @@ function App() {
   return (
     <> {/* ← Fragment */}
       <Header />
+      <main>
+        <h2>Utilisation des events</h2>
+        <EventDemo />
+      </main>
     </>
   )
 }
