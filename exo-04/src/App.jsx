08e1214155b78517dbc752c9d8ee8cee3abb517f
Della Duck 🦆 <aurelien.strimelle@bstorm.be>
2025-11-26 13:14:26 +0100

    Exo 04 - Calculatrice

---
+++ exo-04/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CalculatriceV1 from './components/Calculatrice/CalculatriceV1'
+import CalculatriceV2 from './components/Calculatrice/CalculatriceV2'
+
+function App() {
+
+  return (
+    <>
+    <h1>Exo 04 - Calculatrice</h1>
+    {/* <CalculatriceV1 /> */}
+    <CalculatriceV2 />
+    </>
+  )
+}
+
+export default App
