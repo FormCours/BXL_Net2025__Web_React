dcd9d5f55dcc1591ea6e88a770c094780ab10b10
Zaza Vanderquack <aurelien.strimelle@bstorm.be>
2025-11-27 15:09:15 +0100

    Exo 05 - Flip logo !

--- exo-05/src/ui/Header/Header.jsx
+++ exo-05/src/ui/Header/Header.jsx
@@ -5,7 +5,19 @@ export default function Header() {
 
     return (
         <header className={style['header']}>
-            <img className={style['header-img']} src={zaza} alt='Zaza' />
+            <div className={style['logo-container']}>
+                <div className={style['logo']}>
+                    <div className={style['logo-front']}>
+                        <img className={style['header-img']} src={zaza} alt='Zaza' />
+                    </div>
+                    <div className={style['logo-back']}>
+                        <img className={style['header-img']} src={zaza} alt='Zaza' />
+                    </div>
+                    <div className={style['logo-edge-front']}></div>
+                    <div className={style['logo-edge']}></div>
+                    <div className={style['logo-edge-back']}></div>
+                </div>
+            </div>
             <h1>Todo List</h1>
         </header>
     )
